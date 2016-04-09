446b725d0437a1367e5b3b797fd19cad55181db8
1533165394 <1533165394@qq.com>
2016-04-09 17:21:31 +0800

    git init

--- course-layout/src/scripts/veiws/index.js
+++ course-layout/src/scripts/veiws/index.js
@@ -1,3 +1,71 @@
-var myscroll=require("../pull-to-refresh-master/lib/iscroll-probe.js");
-var $=require("../pull-to-refresh-master/lib/jquery.js");
-var myscroll=new myscroll("section");
+var myScroll = require("iScroll/iscroll-probe.js");
+var $ = require("pull/jquery-1.11.3.js");
+var myiScroll = new myScroll("section",{
+
+});
+
+    var myScroll = new myScroll('section', {
+        probeType: 3,
+        mouseWheel: true
+    });
+    myScroll.scrollBy(0, -38);
+
+    var head = $('.head img'),
+        topImgHasClass = head.hasClass('up');
+    var foot = $('.foot img'),
+        bottomImgHasClass = head.hasClass('down');
+    myScroll.on('scroll', function () {
+        var y = this.y,
+            maxY = this.maxScrollY - y;
+        if (y >= 0) {
+            !topImgHasClass && head.addClass('up');
+            return '';
+        }
+        if (maxY >= 0) {
+            !bottomImgHasClass && foot.addClass('down');
+            return '';
+        }
+    });
+
+    myScroll.on('scrollEnd', function () {
+        if (this.y >= -100 && this.y < 0) {
+            myScroll.scrollTo(0, -38);
+            head.removeClass('up');
+        } else if (this.y >= 0) {
+            head.attr('src', 'images/img/ajax-loader.gif');
+            //TODO ajax下拉刷新数据
+
+            setTimeout(function () {
+                myScroll.scrollTo(0, -38);
+                head.removeClass('up');
+                head.attr('src', 'images/img/arrow.png');
+            }, 1000);
+        }
+
+        var maxY = this.maxScrollY - this.y;
+        if (maxY > -100 && maxY < 0) {
+            var self = this;
+            myScroll.scrollTo(0, self.maxScrollY + 100);
+            foot.removeClass('down')
+        } else if (maxY >= 0) {
+            foot.attr('src', 'images/img/ajax-loader.gif');
+            //TODO ajax上拉加载数据
+
+
+            var self = this;
+            setTimeout(function () {
+                $('.foot').before(
+                    '<div class="main1"><div class="main_logo"><img class="pic1" src="images/img/01.png"/></div><div class="main_section"><div>Remixlive - Play loops on pads.</div><div class="paddtop13">音乐</div><div class="star yo-ico">&#xe673;&#xe673;&#xe673;&#xe673;&#xe611;<span>(52)</span></div></div><div class="main_price"><div class="price">获取</div></div></div>'+
+                    '<div class="item">add 2</div>'+
+                    '<div class="item">add 3</div>'+
+                    '<div class="item">add 4</div>'+
+                    '<div class="item">add 5</div>'
+                );
+                myScroll.refresh();
+
+                myScroll.scrollTo(0, self.y + 100);
+                foot.removeClass('down');
+                foot.attr('src', 'images/img/arrow.png');
+            }, 1000);
+        }
+    })
