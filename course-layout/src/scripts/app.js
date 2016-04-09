d6be0a2cabf99dc98bbc806b16d58cebf61a009f
1533165394 <1533165394@qq.com>
2016-04-09 11:03:11 +0800

    git init

---
+++ course-layout/src/scripts/app.js
@@ -0,0 +1,1 @@
+require("veiws/index.js");
---
+++ course-layout/src/scripts/veiws/index.js
@@ -0,0 +1,3 @@
+var myscroll=require("../pull-to-refresh-master/lib/iscroll-probe.js");
+var $=require("../pull-to-refresh-master/lib/jquery.js");
+var myscroll=new myscroll("section");
